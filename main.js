ade940e2fc35d8a9a58e411b5d79fb87cdb73a4a
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-06-07 01:55:07 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//Create a function show your name in HTML document
+function myName(name){ 
+    var name = "Amira Khaled";
+    return name;
+  }
+  document.getElementById("name").innerHTML = myName();
